b826cc023d5a19846ddd09d709f4a649291a0083
Jean <jeanlucafp@gmail.com>
2019-07-10 23:58:46 -0300

    first commit

---
+++ jaguar-envios/src/app/api.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import axios, { AxiosStatic, AxiosInstance } from 'axios'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseUrl: string = 'http://localhost:8080'
+  private instance: AxiosInstance
+
+  constructor() {
+    this.instance = axios.create({ baseURL: this.baseUrl })
+  }
+
+  public async atualizarToken(token: string) {
+    return await this.instance.post(`/api-wha-token-update`, { token })
+  }
+
+  public async getToken() {
+    return await this.instance.get(`/token`)
+  }
+
+  public async getCredit() {
+    return await this.instance.get(`/credit`)
+  }
+
+  public async enviarMailing(mailing) {
+    let formData = new FormData()
+    formData.append('file', mailing)
+    return await this.instance.post(`/integracao`,
+      formData)
+  }
+
+  public iniciarDisparos() {
+    this.instance.post(`/iniciar-fila-desconhecidos`)
+  }
+
+}
