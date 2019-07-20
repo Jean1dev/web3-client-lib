9c48825f0b24cb1454890dd15b44698fb897ce6f
Jean <jeanlucafp@gmail.com>
2019-07-20 18:33:45 -0300

    implementado chat

--- jaguar-envios/src/app/app.module.ts
+++ jaguar-envios/src/app/app.module.ts
@@ -9,10 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:3007', options: {} };
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    SocketIoModule.forRoot(config),
+    AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +27,4 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- jaguar-envios/src/app/tab2/tab2.page.ts
+++ jaguar-envios/src/app/tab2/tab2.page.ts
@@ -1,5 +1,8 @@
 import { ApiService } from './../api.service';
 import { Component } from '@angular/core';
+import { Socket } from 'ng-socket-io';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -13,8 +16,20 @@ export class Tab2Page {
 
   public listEnvios: any
   public segment: string;
+  public messages = [];
+  public nickname: string = ""
 
-  constructor(public api: ApiService) { }
+  constructor(
+    public api: ApiService,
+    private socket: Socket,
+    public nav: Router) {
+    this.prepareSocketClient()
+
+    this.getMessages().subscribe(message => {
+      console.log(message)
+      this.messages.push(message);
+    });
+  }
 
   ionViewDidEnter() {
     this.segment = "enviados"
@@ -24,13 +39,31 @@ export class Tab2Page {
     })
   }
 
+  public openChat(messageDetail) {
+    this.nav.navigate(['chat'], {queryParams: messageDetail})
+  }
+
   private prepareObjectList(list) {
     this.listEnvios = list.map((item: any) => {
       return {
         status: item.detalhes.success,
         message: item.mensagem_enviada,
-        from : item.telefone_enviado
+        from: item.telefone_enviado
       }
     })
+  }
+
+  private prepareSocketClient() {
+    this.socket.connect();
+    //this.socket.emit('send-server', "conectado")
+  }
+
+  private getMessages() {
+    let observable = new Observable(observer => {
+      this.socket.on('message', (data) => {
+        observer.next(data);
+      });
+    })
+    return observable;
   }
 }
